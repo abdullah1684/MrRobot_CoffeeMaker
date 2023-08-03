80b71cd7c386d21fdc61d555a6d4c6a4c787d6c5
Abdullah Fawzi <abdullahfawzi1684@gmail.com>
2023-08-03 15:55:41 +0300

    cli done

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:coffee_maker/screens/main_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
