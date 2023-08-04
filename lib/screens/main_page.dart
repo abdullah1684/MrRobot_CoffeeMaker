c74ee416a70334884062c0d3a0d5689a9d28fa0e
Abdullah Fawzi <abdullahfawzi1684@gmail.com>
2023-08-04 17:37:05 +0300

    Update main_page.dart

--- lib/screens/main_page.dart
+++ lib/screens/main_page.dart
@@ -1,10 +1,7 @@
-import 'dart:convert';
-
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
+
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 
 final _firebase = FirebaseAuth.instance;
 final DatabaseReference database = FirebaseDatabase.instance.reference();
@@ -16,10 +13,10 @@ class MainPage extends StatefulWidget {
   State<MainPage> createState() => _MainPageState();
 }
 
-final String path = '/make/bool';
+const String path = '/make/bool';
 
 sendBooleanValue(bool value) async {
-  final user = await _firebase.signInWithEmailAndPassword(
+  await _firebase.signInWithEmailAndPassword(
       email: "test@gmail.com", password: "12345678");
 
   await database.child(path).set(value).then((_) {
@@ -33,40 +30,43 @@ class _MainPageState extends State<MainPage> {
   var _isCoffee = false;
   @override
   Widget build(BuildContext context) {
+    final deviceData = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Coffee Hacker'),
       ),
-      body: Stack(
-        children: [
-          Container(
-            height: 150,
-            width: double.infinity,
-            child: Image.asset(
-              'assets/images/coffeeMaker.jpg',
-            ),
-          ),
-          Center(
-            child: ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  _isCoffee = !_isCoffee;
-                });
-                sendBooleanValue(_isCoffee);
-              },
-              child: SizedBox(
-                width: 120,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text('Make Coffee!'),
-                    Icon(Icons.coffee),
-                  ],
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+          Color.fromARGB(255, 206, 193, 188),
+          Color.fromARGB(255, 119, 94, 85),
+        ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+        width: deviceData.width,
+        height: deviceData.height,
+        child: Stack(
+          children: [
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _isCoffee = !_isCoffee;
+                  });
+                  sendBooleanValue(_isCoffee);
+                },
+                child: SizedBox(
+                  width: deviceData.width * 0.3,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Make Coffee!'),
+                      Icon(Icons.coffee),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
